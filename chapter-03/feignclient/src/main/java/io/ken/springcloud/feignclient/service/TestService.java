d2b4fa34a24361ad649f5dba2b423fdd30a6297b
ken <mail@keshuang.net>
2018-04-19 21:37:22 +0800

    服务消费者（Feign）

---
+++ chapter-03/feignclient/src/main/java/io/ken/springcloud/feignclient/service/TestService.java
@@ -0,0 +1,27 @@
+package io.ken.springcloud.feignclient.service;
+
+import io.ken.springcloud.feignclient.model.Plus;
+import io.ken.springcloud.feignclient.model.Result;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "testservice")
+public interface TestService {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    String indexService();
+
+    @RequestMapping(value = "/plus", method = RequestMethod.GET)
+    Result plusService(@RequestParam(name = "numA") int numA, @RequestParam(name = "numB") int numB);
+
+    @RequestMapping(value = "/plus", method = RequestMethod.POST, consumes = "application/json")
+    Result plusabService(Plus plus);
+
+    @RequestMapping(value = "/plus2", method = RequestMethod.POST)
+    Result plus2Service(@RequestBody Plus plus);
+
+
+}
