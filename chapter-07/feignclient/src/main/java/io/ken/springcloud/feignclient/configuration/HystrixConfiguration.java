acdecff6a9d0b16289888b9a05c1cf904e765020
ken <mail@keshuang.net>
2018-07-03 18:57:51 +0800

    服务网关（Zuul）

---
+++ chapter-07/feignclient/src/main/java/io/ken/springcloud/feignclient/configuration/HystrixConfiguration.java
@@ -0,0 +1,28 @@
+package io.ken.springcloud.feignclient.configuration;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HystrixConfiguration {
+
+    @Bean(name = "hystrixRegistrationBean")
+    public ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(
+                new HystrixMetricsStreamServlet(), "/hystrix.stream");
+        registration.setName("hystrixServlet");
+        registration.setLoadOnStartup(1);
+        return registration;
+    }
+
+    @Bean(name = "hystrixForTurbineRegistrationBean")
+    public ServletRegistrationBean servletTurbineRegistrationBean() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(
+                new HystrixMetricsStreamServlet(), "/actuator/hystrix.stream");
+        registration.setName("hystrixForTurbineServlet");
+        registration.setLoadOnStartup(1);
+        return registration;
+    }
+}
---
+++ chapter-07/zuul/src/main/java/io/ken/springcloud/zuul/App.java
@@ -0,0 +1,15 @@
+package io.ken.springcloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
